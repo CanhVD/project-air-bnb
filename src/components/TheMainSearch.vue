b2122b910273500a2e1a1bf6c62f8061e9050975
CanhVD <canhvuduc@vnpt.vn>
2023-11-10 11:32:45 +0700

    add homeview

---
+++ src/components/TheMainSearch.vue
@@ -0,0 +1,24 @@
+<template>
+   <div class="main-search-input">
+
+<div class="main-search-input-item location">
+  <div id="autocomplete-container">
+    <input id="autocomplete-input" type="text" placeholder="Location">
+  </div>
+  <a href="#"><i class="fa fa-map-marker"></i></a>
+</div>
+
+<button class="button" onclick="window.location.href='listings-half-screen-map-list.html'">Search</button>
+
+</div>
+</template>
+
+<script lang="js">
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
